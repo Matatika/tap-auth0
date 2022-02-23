42d54e6a5372d66cbb13a90c3c2a724bc91953a4
ReubenFrankel <rfrankel@matatika.com>
2022-02-23 14:08:04 +0000

    Add `UsersStream`

--- tap_auth0/tap.py
+++ tap_auth0/tap.py
@@ -5,18 +5,11 @@ from typing import List
 from singer_sdk import Tap, Stream
 from singer_sdk import typing as th  # JSON schema typing helpers
 
-# TODO: Import your custom stream types here:
-from tap_auth0.streams import (
-    Auth0Stream,
-    UsersStream,
-    GroupsStream,
-)
+from tap_auth0 import streams
 
-# TODO: Compile a list of custom stream types here
-#       OR rewrite discover_streams() below with your custom logic.
+
 STREAM_TYPES = [
-    UsersStream,
-    GroupsStream,
+    streams.UsersStream,
 ]
 
 
